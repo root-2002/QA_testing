35b4cb468166d54a3be9cdf58e7cec2c31404f61
Malakmousa99 <116799883+Malakmousa99@users.noreply.github.com>
2024-03-26 00:11:28 +0200

    search-bar-tests

--- cypress/e2e/search_bar_test/searchBar.cy.js
+++ cypress/e2e/search_bar_test/searchBar.cy.js
@@ -14,5 +14,43 @@ describe("TC-0011 : Test the output of exist value",()=>{
     cy.visit('https://stacksinfo.web.app/');
     cy.get('.search-bar').type("Nablus and Backend")
      cy.get('.Not-found-container').should('not.contain', 'No results available.');
-  })  
-})
+  }); 
+});
+
+
+
+describe('TC-006:Search for Non-Existent Value' , ()=>{
+  it('Should return No results available',()=>{
+    cy.visit('https://stacksinfo.web.app/');
+    cy.get('.search-bar').type('car');
+    cy.contains('Oops, No Matches Found :(').should('be.visible');
+  });
+  });
+  
+  describe('TC-007: Search for Non-Existent Value', () => {
+    it('Displays "No Matches Found" for Injection Query', () => {
+      cy.visit('https://stacksinfo.web.app');
+      cy.get('.search-bar').type("SELECT * FROM users WHERE username = '' OR '1'='1' AND password = 'input_password';");
+      cy.contains('Oops, No Matches Found :(').should('be.visible');
+    });
+  });
+  
+  
+  describe('TC-008:Search for Frontend Companies', () => {
+    it('Should return companies operating in the field of Frontend', () => {
+      cy.visit('https://stacksinfo.web.app/');
+      cy.get('.search-bar').type("Front");
+      cy.get('.card-container').should('exist');
+      cy.contains('.card-body', 'Frontend').should('exist');
+    });
+  });
+  
+  
+  describe('TC-009: verify from the result if we search about Fro keyword', () => {
+    it('Shows companies operating in the field of Frontend', () => {
+      cy.visit('https://stacksinfo.web.app/');
+      cy.get('.search-bar').type("Fro");
+      cy.get('.card-container').should('exist');
+      cy.contains('.card-body', 'Frontend').should('exist');
+  });
+  });
