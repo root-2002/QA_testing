6bb8956b3ec88c01e174276a1b96c1f535973d92
Amer_maher <4mer.c56@gmail.com>
2024-03-25 17:52:13 +0200

    filter test 13 , 14

--- cypress/e2e/filter_tests/filter_unet.cy.js
+++ cypress/e2e/filter_tests/filter_unet.cy.js
@@ -1,11 +1,43 @@
 const { should } = require("chai");
-describe('TC-0017 : select one item in the filter',()=>{
-    it('shluod display the company have same major',()=>{
+describe('filter',()=>{
+    it('TC-0017:shluod return the company have same major',()=>{
         cy.visit('https://stacksinfo.web.app/');
         cy.get('.filter-button').click()
         cy.get(':nth-child(1) > .dropdown-title > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input').click()
         cy.get(':nth-child(1) > .dropdown-title > :nth-child(1) > :nth-child(2)').click()
         cy.get('.Not-found-container').should('not.contain', 'No results available.');
     })
+    it("TC-0018 : shoud return  mixes company have same options I selected",()=>{
+        cy.visit('https://stacksinfo.web.app/');
+        cy.get('.filter-button').click()
+        cy.get(':nth-child(1) > .dropdown-title > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input').click()
+        cy.get(':nth-child(2) > .dropdown-title > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input').click()
+        cy.get('.Not-found-container').should('not.contain', 'No results available.');
+    })
+    it("TC-0019 : should return the content of checkbox i selected",()=>{
+        cy.visit('https://stacksinfo.web.app/');
+        cy.get('.filter-button').click()
+        cy.get(':nth-child(1) > .dropdown-title > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input').dblclick()
+        cy.get('.Not-found-container').should('not.contain', 'No results available.');
+    })
+    it("TC-0014 : should be responsive", () => {
+        cy.visit('https://stacksinfo.web.app/');
+        cy.viewport(412, 915);
+        cy.get('.filter-button').should('be.visible').click();
+        cy.get('.filters-container').should('be.visible');
+    });
+    it("TC-0013 : should ", () => {
+        cy.visit('https://stacksinfo.web.app/');
     
+        cy.get('.filter-button').click();
+    
+        cy.get(':nth-child(1) > .dropdown-title > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input').click();
+       
+        cy.get('.clear-all-btn').click();
+    
+        cy.get('.selected-filters').should('not.exist');
+    });
+    
+    
+
 })
--- cypress/e2e/search_bar_test/searchBar.cy.js
+++ cypress/e2e/search_bar_test/searchBar.cy.js
@@ -1,7 +1,7 @@
 const { should } = require("chai");
 
 describe('TC-005:Search about exist value', () => {
-  it('should return all cards that contain frontend', () => {
+  it.skip('should return all cards that contain frontend', () => {
     cy.visit('https://stacksinfo.web.app/');
     cy.get('.search-bar').type("front end")
     cy.get('.card-container').should('exist');
@@ -9,3 +9,10 @@ describe('TC-005:Search about exist value', () => {
     cy.get(':nth-child(1) > .card-body > .simple-slider > .slick-slider > .slick-list > .slick-track > .slick-active > :nth-child(1) > .slider-element').should('have.text', 'Frontend');
   });
 });
+describe("TC-0011 : Test the output of exist value",()=>{
+  it.skip("should return All companies in Nablus and ",()=>{
+    cy.visit('https://stacksinfo.web.app/');
+    cy.get('.search-bar').type("Nablus and Backend")
+     cy.get('.Not-found-container').should('not.contain', 'No results available.');
+  })  
+})
