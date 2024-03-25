27f8ac6be3bdb4e00eeeac7a3b15d22d95ed3af3
Amer_maher <4mer.c56@gmail.com>
2024-03-25 11:34:21 +0200

    small edit

--- cypress/e2e/searchBar.cy.js
+++ cypress/e2e/searchBar.cy.js
@@ -1,13 +1,12 @@
 const { should } = require("chai");
 
-describe('template spec', () => {
-  it('passes', () => {
+describe('TC-005:Search about exist value', () => {
+  it('should return all cards that contain frontend', () => {
     cy.visit('https://stacksinfo.web.app/');
     cy.get('.search-bar').type("front end")
     cy.get('.card-container').should('exist');
     cy.get('.card-container > :nth-child(2)').should('exist');
     cy.get(':nth-child(1) > .card-body > .simple-slider > .slick-slider > .slick-list > .slick-track > .slick-active > :nth-child(1) > .slider-element').should('have.text', 'Frontend');
-
   });
   
 });
