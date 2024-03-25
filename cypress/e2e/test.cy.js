cdbd1d349ee0ceef94b1a53946149ddacdcc6f33
Amer_maher <4mer.c56@gmail.com>
2024-03-25 11:16:54 +0200

    small edit

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -6,6 +6,8 @@ describe('template spec', () => {
     cy.get('.search-bar').type("front end")
     cy.get('.card-container').should('exist');
     cy.get('.card-container > :nth-child(2)').should('exist');
+    cy.get(':nth-child(1) > .card-body > .simple-slider > .slick-slider > .slick-list > .slick-track > .slick-active > :nth-child(1) > .slider-element').should('have.text', 'Frontend');
+
   });
   
 });
