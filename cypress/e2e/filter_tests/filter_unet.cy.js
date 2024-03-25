58a5f628166903cf8219ed53c0eadcb62dc425d3
Amer_maher <4mer.c56@gmail.com>
2024-03-25 17:54:38 +0200

    filter test cases 13,14

--- cypress/e2e/filter_tests/filter_unet.cy.js
+++ cypress/e2e/filter_tests/filter_unet.cy.js
@@ -21,7 +21,23 @@ describe('TC-0017 : select one item in the filter',()=>{
         cy.get(':nth-child(1) > .dropdown-title > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input').dblclick()
         cy.get('.Not-found-container').should('not.contain', 'No results available.');
     })
-    
+it("TC-0014 : should be responsive", () => {
+    cy.visit('https://stacksinfo.web.app/');
+    cy.viewport(412, 915);
+    cy.get('.filter-button').should('be.visible').click();
+    cy.get('.filters-container').should('be.visible');
+});
+it("TC-0013 : should ", () => {
+    cy.visit('https://stacksinfo.web.app/');
+
+    cy.get('.filter-button').click();
+
+    cy.get(':nth-child(1) > .dropdown-title > :nth-child(1) > .MuiButtonBase-root > .PrivateSwitchBase-input').click();
+   
+    cy.get('.clear-all-btn').click();
+
+    cy.get('.selected-filters').should('not.exist');
+});
     
 })
 
